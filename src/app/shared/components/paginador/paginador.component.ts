325e02db65efa500f001fec33327f8ddd02d4674
EmanuelFMontenegro <emanuel_efm@hotmail.com>
2024-07-29 10:11:51 -0300

    new componente paginador

---
+++ src/app/shared/components/paginador/paginador.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+
+@Component({
+  selector: 'app-paginador',
+  templateUrl: './paginador.component.html',
+  styleUrls: ['./paginador.component.sass']
+})
+export class PaginadorComponent {
+  @Input() length: number = 0;
+  @Input() pageSize: number = 10;
+  @Input() pageIndex: number = 0;
+  @Input() pageSizeOptions: number[] = [5, 10, 25, 100];
+  @Output() page: EventEmitter<PageEvent> = new EventEmitter<PageEvent>();
+
+  itemsPerPageLabel = 'Artículos por página:';
+  nextPageLabel = 'Siguiente página';
+  previousPageLabel = 'Página anterior';
+
+  handlePageEvent(event: PageEvent) {
+    this.page.emit(event);
+  }
+}
