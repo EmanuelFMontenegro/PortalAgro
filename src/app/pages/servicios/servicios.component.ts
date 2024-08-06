fa3bf56c23b8fb819344a37640c38ae9156b2132
EmanuelFMontenegro <emanuel_efm@hotmail.com>
2024-08-06 17:35:33 -0300

    arreglo de service

--- src/app/pages/servicios/servicios.component.ts
+++ src/app/pages/servicios/servicios.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { DashboardBackOfficeService } from '../dashboard-backoffice/dashboard-backoffice.service';
-import { TipoLabel, DataView } from 'src/app/shared/components/miniatura-listado/miniatura.model';
+import {
+  TipoLabel,
+  DataView,
+} from 'src/app/shared/components/miniatura-listado/miniatura.model';
 import { ServiciosService } from 'src/app/services/servicios.service';
 import { Router } from '@angular/router';
 import { ApiService } from 'src/app/services/ApiService';
@@ -12,64 +15,92 @@ import { jwtDecode } from 'jwt-decode';
 @Component({
   selector: 'app-servicios',
   templateUrl: './servicios.component.html',
-  styleUrls: ['./servicios.component.scss']
+  styleUrls: ['./servicios.component.scss'],
 })
 export class ServiciosComponent {
-
   constructor(
     public serviciosService: ServiciosService,
     private apiService: ApiService,
     private authService: AuthService,
     private router: Router,
     private toastr: ToastrService,
-    public dashboardBackOffice: DashboardBackOfficeService) {
-    this.dashboardBackOffice.dataTitulo.next({ titulo: 'Solicitudes y servicios', subTitulo: '' })
+    public dashboardBackOffice: DashboardBackOfficeService
+  ) {
+    this.dashboardBackOffice.dataTitulo.next({
+      titulo: 'Solicitudes y servicios',
+      subTitulo: '',
+    });
   }
-  urlBase = 'dashboard-backoffice'
-  opcionSeleccionada: any
-  listado: any[] = []
-  dataView: DataView[] = []
-  lotesOriginal: any[] = []
-  cultivos: any[] = []
+  urlBase = '';
+  opcionSeleccionada: any;
+  listado: any[] = [];
+  dataView: DataView[] = [];
+  lotesOriginal: any[] = [];
+  cultivos: any[] = [];
   backOffice = false;
 
-  chacras: any[] = [] // ELIMINAR
+  chacras: any[] = []; // ELIMINAR
 
   ngOnInit(): void {
     this.setUrlVerMas();
-    this.backOffice ? this.getServicios() : this.getServiciosByProductor()
+    this.backOffice ? this.getServicios() : this.getServiciosByProductor();
   }
 
   setUrlVerMas() {
     this.getCultivos();
-    this.urlBase = this.backOffice ? 'dashboard-backoffice' : 'dashboard'
+    this.urlBase = this.backOffice ? 'dashboard-backoffice' : 'dashboard';
     this.dataView = [
       { label: 'Estado', field: 'status.name', tipoLabel: TipoLabel.span },
       { label: 'Chacra', field: 'nombreChacra', tipoLabel: TipoLabel.span },
-      { label: 'Plantación', field: 'cropDescripcion', tipoLabel: TipoLabel.span },
+      {
+        label: 'Plantación',
+        field: 'cropDescripcion',
+        tipoLabel: TipoLabel.span,
+      },
       { label: 'Lotes', field: 'cantidadLotes', tipoLabel: TipoLabel.span },
       { label: 'Hectarias', field: 'hectare', tipoLabel: TipoLabel.span },
-      { label: 'Localidad de la chacra', field: 'location.name', tipoLabel: TipoLabel.span },
-      { label: 'Tecnico asignado', field: 'jobOperator', tipoLabel: TipoLabel.span },
-      { label: 'Contacto técnico', field: 'jobTechnical', tipoLabel: TipoLabel.span },
-      { label: 'Piloto asignado', field: 'function', tipoLabel: TipoLabel.span },
+      {
+        label: 'Localidad de la chacra',
+        field: 'location.name',
+        tipoLabel: TipoLabel.span,
+      },
+      {
+        label: 'Tecnico asignado',
+        field: 'jobOperator',
+        tipoLabel: TipoLabel.span,
+      },
+      {
+        label: 'Contacto técnico',
+        field: 'jobTechnical',
+        tipoLabel: TipoLabel.span,
+      },
+      {
+        label: 'Piloto asignado',
+        field: 'function',
+        tipoLabel: TipoLabel.span,
+      },
       { label: 'Contacto piloto', field: 'brand', tipoLabel: TipoLabel.span },
-      { label: 'servicio', field: this.urlBase + '/servicios/', tipoLabel: TipoLabel.botonVermas },
-    ]
+      {
+        label: 'servicio',
+        field: this.urlBase + '/servicios/',
+        tipoLabel: TipoLabel.botonVermas,
+      },
+    ];
   }
 
   getServiciosByProductor() {
-    this.serviciosService.getServiciosByProductor()
+    this.serviciosService
+      .getServiciosByProductor()
       .pipe(map((response: any) => this.convertirValores(response.list[0])))
       .subscribe(
         (data: any) => {
           if (data?.length > 0) {
-            this.lotesOriginal = data
-            this.listado = data
+            this.lotesOriginal = data;
+            this.listado = data;
           }
         },
-        error => { }
-      )
+        (error) => {}
+      );
   }
 
   ///// REVISAR SI ELIMINAR  /////////////
@@ -78,8 +109,8 @@ export class ServiciosComponent {
     // await this.getChacras()
     if (valores?.length) {
       valores.forEach((lote: any) => {
-        lote.cantidadLotes = lote.plots.length
-        lote.cropDescripcion = this.getDescripcionPlantacion(lote.typeCrop_id)
+        lote.cantidadLotes = lote.plots.length;
+        lote.cropDescripcion = this.getDescripcionPlantacion(lote.typeCrop_id);
         // lote.nombreChacra = this.getDescripcionChacra(lote.field_id)
       });
     }
@@ -87,11 +118,11 @@ export class ServiciosComponent {
   }
 
   getDescripcionPlantacion(crop_id: number) {
-    return this.cultivos.find(cultivo => cultivo.id == crop_id).name
+    return this.cultivos.find((cultivo) => cultivo.id == crop_id).name;
   }
 
   getDescripcionChacra(field_id: number) {
-    return this.chacras?.find(chacra => chacra?.id == field_id)?.name
+    return this.chacras?.find((chacra) => chacra?.id == field_id)?.name;
   }
 
   getChacras() {
@@ -102,7 +133,7 @@ export class ServiciosComponent {
         (response) => {
           if (response.list && response.list.length > 0) {
             this.chacras = response.list[0];
-            resolve(this.chacras)
+            resolve(this.chacras);
           } else {
             resolve([]);
             console.error('La lista de campos está vacía o no está definida');
@@ -113,35 +144,35 @@ export class ServiciosComponent {
           resolve([]);
         }
       );
-    })
+    });
   }
 
   /////////////////////////////////
 
   getServicios() {
     this.serviciosService.getServicios().subscribe(
-      data => {
-        console.log(data)
+      (data) => {
+        console.log(data);
         if (data?.list[0].length > 0) {
-          this.lotesOriginal = data?.list[0]
-          this.listado = data?.list[0]
+          this.lotesOriginal = data?.list[0];
+          this.listado = data?.list[0];
         }
       },
-      error => { }
-    )
+      (error) => {}
+    );
   }
 
   filtrar() {
     if (!this.opcionSeleccionada) {
       this.toastr.info('Selecione un tipo de cultivo.', 'Información');
     } else {
-      let crop_id: number = this.opcionSeleccionada
-      this.listado = this.lotesOriginal.filter(x => x.typeCrop_id == crop_id)
+      let crop_id: number = this.opcionSeleccionada;
+      this.listado = this.lotesOriginal.filter((x) => x.typeCrop_id == crop_id);
     }
   }
 
   nuevo() {
-    this.router.navigate([this.urlBase + '/servicios/add-edt-servicio']);
+    this.router.navigate([this.urlBase + '/servicios/nuevo']);
   }
 
   getCultivos() {
@@ -157,5 +188,4 @@ export class ServiciosComponent {
       }
     );
   }
-
 }
