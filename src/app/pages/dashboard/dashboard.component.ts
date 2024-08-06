9b7650561eaa4f81d350ffdab0c067bb8f6352a2
silicon <daniel.romero@siliconmisiones.gob.ar>
2024-08-06 17:22:47 -0300

    dashboard-backoffice structure

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -4,16 +4,34 @@ import { BreakpointObserver } from '@angular/cdk/layout';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { SidenavService } from 'src/app/services/sidenav.service';
+interface Ellipses {
+  left: string;
+  top: string;
+  right: string;
+  bottom: string;
+}
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.sass']
 })
+
+
+
 export class DashboardComponent implements AfterViewInit {
   @ViewChild('sidenav') sidenav!: MatSidenav;
   sidenavOpened: boolean = false;
   isScreenSmall = false;
   menuItems: any[] = [];
+  svgPath = 'menu'
+  ellipses: Ellipses = {
+    left: 'assets/img/footer/ellipse_white.svg',
+    top: 'assets/img/footer/ellipse_white_mobile.svg',
+    right: 'assets/img/footer/ellipse_green.svg',
+    bottom: 'assets/img/footer/ellipse_green_mobile.svg'
+  } 
+  background = '#3BA549'
   constructor(
     private breakpointObserver: BreakpointObserver,
     private router: Router,
@@ -52,7 +70,7 @@ export class DashboardComponent implements AfterViewInit {
     this.http.get<any>('../../assets/json/menu-dashboard.json').subscribe(data => {
       this.menuItems = data.menuItems;
     });
-  }
+  } 
   onSidenavToggle(opened: boolean): void {
     if (!opened) {
       this.sidenavService.changeState(false);
