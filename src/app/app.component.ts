c711de88c29cd52c7753d22bc31cd48b3a85466a
silicon <daniel.romero@siliconmisiones.gob.ar>
2024-09-30 16:39:49 -0300

    se agrego variables para el tipo de login usado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { setupUnloadListener } from './upload-handler-app';
+import { ThemeService } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -8,16 +7,13 @@ import { setupUnloadListener } from './upload-handler-app';
   styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  loginForm = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', Validators.required),
-  });
-  ngOnInit() {}
-
-  iniciarSesion() {
-    if (this.loginForm.valid) {
-      // Implementar lógica de inicio de sesión aquí
-      console.log('Inicio de Sesión Correcto !!!', this.loginForm.value);
+  constructor(private themeService: ThemeService) {
+  }
+   ngOnInit() {
+    const savedTheme = this.themeService.getTheme();
+    if (savedTheme) {
+      this.themeService.applyTheme(savedTheme);
     }
+    
   }
 }
